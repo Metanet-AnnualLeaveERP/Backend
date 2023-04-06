e63c2178303e6eeffbec7965d6bc09aa0d9bb61f
백성준 <qortjdwns120712@gmail.com>
2023-04-06 19:19:06 +0900

    INIT Settings

---
+++ src/main/java/com/meta/ale/AleApplication.java
@@ -0,0 +1,12 @@
+package com.meta.ale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AleApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AleApplication.class, args);
+	}
+
+}
