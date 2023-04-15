8c129a2ff2f1c7d1ae31a4ed86bf34443099bc4d
백성준 <qortjdwns120712@gmail.com>
2023-04-15 17:44:17 +0900

    FEAT: 총 휴가 수 조회

--- src/main/java/com/meta/ale/service/VcTypeTotalService.java
+++ src/main/java/com/meta/ale/service/VcTypeTotalService.java
@@ -1,9 +1,11 @@
 package com.meta.ale.service;
 
+import com.meta.ale.domain.VcTypeDto;
 import com.meta.ale.domain.VcTypeTotalDto;
 
 public interface VcTypeTotalService {
 
     boolean updateVcTotalCount(VcTypeTotalDto vcTypeTotalDto);
 
+    VcTypeDto getVcTotalByTypeAndEmpId();
 }
--- src/main/java/com/meta/ale/service/VcTypeTotalServiceImpl.java
+++ src/main/java/com/meta/ale/service/VcTypeTotalServiceImpl.java
@@ -1,13 +1,23 @@
 package com.meta.ale.service;
 
+import com.meta.ale.domain.VcTypeDto;
 import com.meta.ale.domain.VcTypeTotalDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class VcTypeTotalServiceImpl implements VcTypeTotalService{
+
+
     @Override
     public boolean updateVcTotalCount(VcTypeTotalDto vcTypeTotalDto) {
 
         return false;
+    }
+
+    @Override
+    public VcTypeDto getVcTotalByTypeAndEmpId() {
+        return null;
     }
 }
