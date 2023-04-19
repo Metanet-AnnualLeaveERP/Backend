42c8ec66e2ec006cf73e6bf8853a30f23fa1b74a
ysb4642 <ysb4642@naver.com>
2023-04-19 17:53:42 +0900

    FEAT: modify api 수정

--- src/main/java/com/meta/ale/domain/UserDto.java
+++ src/main/java/com/meta/ale/domain/UserDto.java
@@ -3,7 +3,6 @@ package com.meta.ale.domain;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
